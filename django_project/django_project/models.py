8f771818cabec78d65261fb431576b0632fdf45a
Stefan Dimitrov <elwebmaster@gmail.com>
2015-03-25 01:32:57 -0400

    added Task owner.

--- django_project/django_project/models.py
+++ django_project/django_project/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     id = models.AutoField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
     due = models.DateTimeField()
-    #owner = models.ForeignKey(User, related_name='username')
+    owner = models.ForeignKey(User, related_name='username')
     title = models.CharField(max_length=250, blank=True, default='')
     description = models.TextField()
 
