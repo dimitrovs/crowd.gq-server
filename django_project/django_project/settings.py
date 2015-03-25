45cc1549c028e98837481ad822833b5c9b46b5cc
Stefan Dimitrov <elwebmaster@gmail.com>
2015-03-25 00:39:47 -0400

    Added modules.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -53,8 +53,9 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'PAGE_SIZE': 10
 }
 
 ROOT_URLCONF = 'django_project.urls'
